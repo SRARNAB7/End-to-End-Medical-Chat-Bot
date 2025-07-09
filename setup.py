912412b71b1c3f09855f1741ce20ee96609144dc
Arnab Sutar <Arnab@Subratas-MBP.attlocal.net>
2025-07-09 12:07:25 -0700

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name= 'Generative AI Project',
+    version='0.0.0',
+    author= 'Arnab Sutar',
+    author_email='srarnab7@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+)
